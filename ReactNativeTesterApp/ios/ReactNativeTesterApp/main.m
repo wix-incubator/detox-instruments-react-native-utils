e6fd1bd1d60a99b3bca1b920b53f2d4c608cac48
Leo Natan <leo.natan@outlook.com>
2019-10-29 20:19:30 +0200

    Upgrade demo project to 0.61.2 with CocoaPods

--- ReactNativeTesterApp/ios/ReactNativeTesterApp/main.m
+++ ReactNativeTesterApp/ios/ReactNativeTesterApp/main.m
@@ -6,11 +6,25 @@
  */
 
 #import <UIKit/UIKit.h>
-
+@import DTXProfiler;
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
 	@autoreleasepool {
+//		DTXMutableProfilingConfiguration* config = [DTXMutableProfilingConfiguration defaultProfilingConfiguration];
+//		config.recordReactNativeBridgeData = YES;
+//		config.recordInternalReactNativeEvents = YES;
+//		config.recordingFileURL = [NSURL fileURLWithPath:@"/Users/lnatan/Desktop/Testing.dtxrec"];
+//		
+//		DTXProfiler* _ = [DTXProfiler new];
+//		[_ continueProfilingWithConfiguration:config];
+//		
+//		dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//			[_ stopProfilingWithCompletionHandler:^(NSError * _Nullable error) {
+//				NSLog(@"👹");
+//			}];
+//		});
+		
 		return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
 	}
 }
